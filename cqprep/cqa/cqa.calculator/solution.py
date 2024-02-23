2ab03a52e8eee256f44d06892ed28b5ce838d86a
vladb <vlad.bondar@frc5113.com>
2024-02-23 17:56:53 +0000

    cqa budget + default is now regular for autotest

---
+++ cqprep/cqa/cqa.calculator/solution.py
@@ -0,0 +1,47 @@
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN
+from math import floor
+
+##### FUNCTIONS ######
+
+# standard rounding
+# n - number to round
+# d - decimal places
+def round_standard(n, d):
+    return floor(n*(10**d)+0.5)/10**d
+
+
+# advanced rounding (when needed, like with "are we on budget")
+# n - number to round
+# d - decimal places
+# m - rounding mode
+def round_advanced(n, d, m = ROUND_HALF_UP):
+    # quantize takes 0.1, 0.01 etc.
+    return Decimal(n).quantize(Decimal(str(10**-d)), rounding=m)
+
+# get ascii position of a character (0 (a) - 25 (z))
+def ascii_pos(l, cap = True):
+    return ord(l) - ord("A" if cap else "a")
+    
+##### FUNCTION TESTS ######
+# print(ascii_pos("z", False))
+# print(round_advanced(1.5, 0))
+
+
+######## INPUT ##########
+num_lines = int(input())
+data = []
+for n in range(num_lines):
+    # process the line here, append to data
+    inp = input()
+    r = inp.split(" ")
+    data.append([int(r[0]), r[1], int(r[2])])
+
+output = []
+
+# DO PROCESSING HERE
+for point in data:
+    reg = round_advanced(eval(f"{point[0]}{point[1]}{point[2]}"), 1)
+    rev = round_advanced(eval(f"{point[2]}{point[1]}{point[0]}"), 1)
+    output.append(f"{reg} {rev}")
+
+print("\n".join(output))
